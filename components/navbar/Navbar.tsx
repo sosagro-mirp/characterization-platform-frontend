0a76e8964cc2cf72c1f7866830a87115b5a2b5d3
santiagoSuarez219 <santiago8628@gmail.com>
2026-04-06 13:30:05 -0500

    Add icon

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link';
 import { useState } from 'react';
+import Image from "next/image"
 
 export const Navbar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -18,8 +19,15 @@ export const Navbar = () => {
         <>
             <nav className='fixed top-0 left-0 w-full z-50 bg-white border-b border-gray-200'>
                 <div className='flex items-center justify-between w-full px-6 lg:px-12 py-4'>
-                    <Link href="/" className='flex gap-2 items-center text-green-900'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="currentColor" className="icon icon-tabler icons-tabler-filled icon-tabler-leaf"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M3.055 14.328l-.018 -.168l-.004 -.043a11 11 0 0 1 -.047 -1.12c.018 -6.29 4.29 -9.997 13 -9.997h4.014a1 1 0 0 1 1 1l-.002 2.057c-.498 8.701 -4.74 12.943 -11.998 12.943h-2.631a16 16 0 0 0 -.375 2.11a1 1 0 1 1 -1.988 -.22q .174 -1.568 .58 -2.947l-.118 -.146l-.208 -.28l-.157 -.229l-.182 -.293l-.098 -.171l-.065 -.122a6 6 0 0 1 -.397 -.941l-.072 -.237l-.085 -.327l-.057 -.268l-.043 -.242zm8.539 -4.242c-2.845 1.265 -4.854 3.13 -6.108 5.583q .098 .2 .218 .4l.185 .281l.07 .097q .12 .164 .258 .329l.197 .224h.649c1.037 -2.271 2.777 -3.946 5.343 -5.086a1 1 0 0 0 -.812 -1.828" /></svg>
+                    <Link href="/" className='flex gap-3 items-center text-green-900'>
+                        <Image
+                            src="/logo.jpg"
+                            alt="logo"
+                            width={300}
+                            height={300}
+                            className="h-10 w-auto object-contain sm:h-10 md:h-12"
+                        />
+                        {/* <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="currentColor" className="icon icon-tabler icons-tabler-filled icon-tabler-leaf"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M3.055 14.328l-.018 -.168l-.004 -.043a11 11 0 0 1 -.047 -1.12c.018 -6.29 4.29 -9.997 13 -9.997h4.014a1 1 0 0 1 1 1l-.002 2.057c-.498 8.701 -4.74 12.943 -11.998 12.943h-2.631a16 16 0 0 0 -.375 2.11a1 1 0 1 1 -1.988 -.22q .174 -1.568 .58 -2.947l-.118 -.146l-.208 -.28l-.157 -.229l-.182 -.293l-.098 -.171l-.065 -.122a6 6 0 0 1 -.397 -.941l-.072 -.237l-.085 -.327l-.057 -.268l-.043 -.242zm8.539 -4.242c-2.845 1.265 -4.854 3.13 -6.108 5.583q .098 .2 .218 .4l.185 .281l.07 .097q .12 .164 .258 .329l.197 .224h.649c1.037 -2.271 2.777 -3.946 5.343 -5.086a1 1 0 0 0 -.812 -1.828" /></svg> */}
                         <h1 className='text-2xl font-semibold '>SosAgro 4C</h1>
                     </Link>
                     <ul className='hidden lg:flex gap-6 text-gray-700 tracking-tight text-sm'>
